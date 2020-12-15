a1d2e9fc20b69175382538458ddc57126eefe56b
3217mehul <3217mehul@gmail.com>
2020-12-15 18:39:36 +0530

    changes

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
-const order = document.querySelector('#user');
-const form = document.querySelector('#add-project-form')
-// const boxes = document.getElementsByClassName('col-sm-4 adiv');
-var data
+const Enquiry = document.querySelector('#Enquiry');
+const Site_Visit = document.querySelector('#Site_Visit');
+const Won = document.querySelector('#Won');
 
-function renderusers(doc)
-{
+const form = document.querySelector('#add-project-form');
+const lists = document.querySelectorAll('.list');
+
+function make_card(doc){
     let card = document.createElement('div');
-    card.className = "card";
+    card.className = "list-item";
     card.style.cssText = 'cursor: pointer'
-    // card.draggable = 'true';
-
+    card.draggable = 'true';
     let card_body = document.createElement('div');
     card_body.className = 'card-body';
     let card_cont = document.createElement('div');
@@ -20,7 +20,6 @@ function renderusers(doc)
     card_row2.className = 'row';
     let hr = document.createElement('hr');
     let br = document.createElement('br');
-
     let d1a = document.createElement('div');
     d1a.className = 'col-4';
     d1a.style.cssText = 'text-align: left';
@@ -33,27 +32,23 @@ function renderusers(doc)
     let d2b = document.createElement('div');
     d2b.className = 'col-4';
     d2b.style.cssText = 'text-align: right';
-    let name = document.createElement('div');
-    let id = document.createElement('div');
-    let req = document.createElement('div');
-    let date = document.createElement('div');
-
+    let Name = document.createElement('div');
+    let Id = document.createElement('div');
+    let Req = document.createElement('div');
+    let Date = document.createElement('div');
     d1a.setAttribute('data-id', doc.id);
     d1b.setAttribute('data-id', doc.id);
     d2a.setAttribute('data-id', doc.id);
     d2b.setAttribute('data-id', doc.id);
-    id.textContent = doc.data().id;
-    name.textContent = doc.data().name;
-    req.textContent = doc.data().req;
-    var obj  = doc.data().date;
-    date.textContent = new Date(obj["seconds"]*1000).toDateString();
-
-
-    d1a.appendChild(id);
-    d1b.appendChild(date);
-    d2a.appendChild(name);
-    d2b.appendChild(req);
-
+    Id.textContent = doc.data().Id;
+    Name.textContent = doc.data().Name;
+    Req.textContent = doc.data().Req;
+    var obj  = doc.data().Date;
+    Date.textContent = new window.Date(obj["seconds"]*1000).toDateString();
+    d1a.appendChild(Id);
+    d1b.appendChild(Date);
+    d2a.appendChild(Name);
+    d2b.appendChild(Req);
     card_row1.appendChild(d1a);
     card_row1.appendChild(d1b);
     card_row2.appendChild(d2a);
@@ -62,48 +57,130 @@ function renderusers(doc)
     card_cont.appendChild(hr);
     card_cont.appendChild(card_row2);
     card_body.appendChild(card_cont);
-    
     card.appendChild(card_body);
-    // cards.push(card);
-    order.appendChild(card);
-    order.appendChild(br);
-
-    
+    return card;
 }
 
-db.collection('Data').get().then(snapshot => {
-    data = snapshot.docs;
-    let j = 10;
-    if(data.length < j)
-        j = data.length
-    for(i=0;i<j;i++)
-        renderusers(data[i]);
+function dragndrop(card,list_i,doc,drag)
+{
+    console.log(list_i);
+    card.addEventListener('dragstart', function(e) {
+        e.stopPropagation();
+        console.log('drag started');
+        console.log(doc.id);
+        setTimeout(function(){
+            card.style.display = 'none';
+        }, 0);
+    });
+    card.addEventListener('dragend', function(e) {
+        e.stopPropagation();
+        console.log('drag ended');
+        console.log(doc.id);
+        setTimeout(function() {
+            card.style.display = 'block';
+        }, 0);
+    });
+    for(let j=0;j<lists.length;j++)
+    {
+        const list = lists[j];
+        list.addEventListener('dragover',function(e){
+            e.stopPropagation();
+            e.preventDefault();
+        })
+        list.addEventListener('dragenter',function(e){
+            e.stopPropagation();
+            e.preventDefault();
+        })
+        list.addEventListener('drop', function(e){
+            e.stopPropagation();
+            this.append(card);
+            if(this.id == "Site_Visit")
+            {
+                db.collection('Site_Visit').add({
+                    Name: doc.data().Name,
+                    Req: doc.data().Req,
+                    Date: doc.data().Date,
+                    Id: 1
+                })
+            }
+            else if(this.id == "Enquiry")
+            {
+                db.collection('Enquiry').add({
+                    Name: doc.data().Name,
+                    Req: doc.data().Req,
+                    Date: doc.data().Date,
+                    Id: 1
+                })
+            }
+            else
+            {
+                db.collection('Won').add({
+                    Name: doc.data().Name,
+                    Req: doc.data().Req,
+                    Date: doc.data().Date,
+                    Id: 1
+                })
+            }
+            db.collection(list_i).doc(doc.id).delete();
+        })
+    }
+}
+
+db.collection('Enquiry').get().then(snapshot => {
+    var data = snapshot.docs;
+    for(i=0;i<data.length;i++)
+    {
+        let card = make_card(data[i]);
+        Enquiry.appendChild(card);
+        let drag = null;
+        dragndrop(card,'Enquiry',data[i],drag);
+    }
+})
+
+db.collection('Won').get().then(snapshot => {
+    var data = snapshot.docs;
+    for(i=0;i<data.length;i++)
+    {
+        let card = make_card(data[i]);
+        Won.appendChild(card);
+        let drag = null;
+        dragndrop(card,'Won',data[i],drag);
+    }
+})
+
+db.collection('Site_Visit').get().then(snapshot => {
+    var data = snapshot.docs;
+    for(i=0;i<data.length;i++)
+    {
+        let card = make_card(data[i]);
+        Site_Visit.appendChild(card);
+        let drag = null;
+        dragndrop(card,'Site_Visit',data[i],drag);
+    }
 })
 
 form.addEventListener('submit', (e) => {
     e.preventDefault();
-    db.collection('Data').add({
-        name: form.name.value,
-        req: form.req.value,
-        date: new Date(),
-        id: 1
+    db.collection('Enquiry').add({
+        Name: form.name.value,
+        Req: form.req.value,
+        Date: new Date(),
+        Id: 1
     })
     form.name.value = '';
     form.req.value = '';
-    
 })
 
-
-idx=10;
-function load()
-{
-    if(idx < data.length)
-    {
-        let j = 10 + idx;
-        if(data.length < j)
-            j = data.length
-        for(i=idx;i<j;i++)
-            renderusers(data[i]);
-        idx+=10;
-    }
-}
+// let idx=10;
+// function load()
+// {
+//     if(idx < data.length)
+//     {
+//         let j = 10 + idx;
+//         if(data.length < j)
+//             j = data.length
+//         for(i=idx;i<j;i++)
+//             renderusers(data[i]);
+//         idx+=10;
+//     }
+// }
